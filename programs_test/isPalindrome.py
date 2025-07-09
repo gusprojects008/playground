49771bb636d5a0137c03f93c4e7ba4874ccce78c
gusprojects008 <gus0100@tutamail.com>
2025-07-09 01:56:58 -0300

    leetcode fixed kkkk wtf

--- programs_test/isPalindrome.py
+++ programs_test/isPalindrome.py
@@ -1,10 +1,23 @@
 class Solution:
-      def isPalindrome(self, x: int): # -> bool:
-          listInts = [number for number in str(x)]
-          if listInts[0] == listInts[-1]:
-             return True
-          else:
+      def isPalindrome(self, x: int) -> bool:
+          # Bad solution
+          #listInts = [number for number in str(x)]
+          #if listInts[0] == listInts[-1]:
+          #   return True
+          #else:
+          #   return False
+
+          # Good solution
+          if x < 10:
              return False
-x = -121
+          result = 0
+          default = x
+          x = abs(x)
+          while x != 0:
+                result = result * 10 + (x % 10)
+                x //= 10
+          return result == default
+
+x = -1421
 SolutionObj = Solution()
 print(SolutionObj.isPalindrome(x))
